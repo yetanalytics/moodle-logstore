4de1acdecb75144032270ef2a39dc8feae92ffdb
Milton Reder <milt@yetanalytics.com>
2024-04-16 15:43:52 -0400

    try name without path

--- tests/utils/object_to_array.php
+++ tests/utils/object_to_array.php
@@ -22,7 +22,7 @@
  * @license   https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace tests\utils;
+namespace TestUtils;
 
 /**
  * Given a deeply nested object representing JSON, make it an array
